5c28c03c6dc0234cd50d0f93982b68de4faf38b0
Ahmed Abdalla <aabdalla@amplify-nation.com>
2016-04-26 16:32:19 -0400

    adds modules for peer and peers getter

---
+++ peer.py
@@ -0,0 +1,60 @@
+import socket
+from torrent import Torrent
+import struct
+
+from bitstring import BitArray
+
+
+class Peer(object):
+    def __init__(self, torrent : Torrent, peer_info : (str, int)):
+        self.torrent = torrent
+        self.peer_info = peer_info
+
+        self.sock = None
+        self.handshake = ''
+        self.is_connected = False
+
+        self.state = None
+        self.is_interested = False
+        self.am_interested = False
+        self.am_chocking = True
+        self.is_interested = False
+
+        self.msg_queue = []
+
+        self.pieces = BitArray(0 * self.torrent.num_pieces)
+
+
+    def is_handshake_valid(self, handshake : bytes, resp : bytes):
+        """
+        <pstrlen><pstr><reserved><info_hash><peer_id>
+        """
+        return handshake[28:48] == resp[28:48]
+
+
+    def connect(self, handshake):
+        sock = socket.socket()
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.setblocking(False)
+
+        try:
+            sock.connect(self.peer_info)
+            sock.send(handshake)
+        except OSError:
+            raise Exception("Failed to connect")
+
+        resp = sock.recv(2**15)
+
+        print(resp)
+
+        if not self.is_handshake_valid(handshake, resp):
+            print("Failed to connect to {} -- invalid handshake".format(self.peer_info))
+        else:
+            print("{} connected successfully".format(self.peer_info))
+            self.is_connected = True
+
+
+    def fileno(self):
+        if not self.sock:
+            raise Exception("TCP connection with peer has not been established")
+        return self.sock.fileno()
---
+++ peers_getter.py
@@ -0,0 +1,45 @@
+import requests
+
+# from bencode import decode
+from bencoder import decode, encode
+
+
+class PeersGetter(object):
+    def __init__(self, torrent):
+        self.torrent = torrent
+
+    def request_peers(self):
+        resp = requests.get(
+            self.torrent.get_announce_url(),
+            params=self.torrent.get_tracker_request_params()
+        )
+        return decode(resp.content)
+
+
+    def parse_peers(self):
+        """
+        Response data can be returned in binary....
+        """
+        peers_resp = self.request_peers()
+        peers_data = peers_resp[b'peers']
+
+
+        peers = []
+
+        # Handle binary peers_data mode
+        if isinstance(peers_data, bytes):
+            for i in range(0, int(len(peers_data)), 6):
+                ip_addr = '.'.join(
+                    [str(byte) for byte in peers_data[i:i+4]]
+                )
+                port = (peers_data[i + 4] * 256) + peers_data[i + 5]
+                peers.append((ip_addr, port))
+
+        elif isinstance(peers_data, dict):
+            #TODO parse dict model for peers list
+            pass
+
+        else:
+            raise Exception("Unable to retrieve peers from torrent response")
+
+        return peers
